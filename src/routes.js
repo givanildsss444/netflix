e856dacda79df26114b5d2df049aabd8b88c9d88
Givanildo <cicerofarias1390gmil@gmail.com>
2025-08-13 09:27:30 -0300

    Netlfix 2024

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import {Routes, Route} from "react-router-dom";
+import Profiles from "./pages/Profiles";
+import Home from "./pages/Home";
+import Config from "./pages/Config";
+
+function MainRoutes(){
+
+    return(
+        <Routes> 
+            <Route path="/" element={<Profiles/>} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/config" element={<Config />} />
+
+        </Routes>
+
+    );
+
+}
+
+export default MainRoutes;
